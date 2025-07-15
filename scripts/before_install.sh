354e53c1f832a648dc7fc8975f2ddff007eb3ee4
Aravindh <113278417+joyson04@users.noreply.github.com>
2025-07-15 18:59:33 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# Create /app directory if it doesn't exist
+if [ ! -d "/app" ]; then
+  echo "Creating /app directory..."
+  mkdir -p /app
+  chown ec2-user:ec2-user /app   # Optional: set owner
+fi
+
 cd /app
 npm install
 npm install -g pm2
